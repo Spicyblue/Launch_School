dbe998904242779341ce7845d28762891efb78ed
Spicyblue <ochulor.okechukwu@gmail.com>
2024-05-29 00:48:28 +0200

    Solution to small problem exercise

--- PY_101/Small_Problems/Easy_3/exercise_09.py
+++ PY_101/Small_Problems/Easy_3/exercise_09.py
@@ -3,3 +3,19 @@
 # write a function that returns that string with all of the non-alphabetic characters replaced by spaces. 
 # If one or more non-alphabetic characters occur in a row, you should only have one space in the result 
 # (i.e., the result string should never have consecutive spaces).
+
+# Solution
+
+def clean_up(string_input):
+    new_string = ""
+
+    for index, char in enumerate(string_input):
+        if char.isalpha():
+            new_string += char
+        elif index == 0 or new_string[-1] != ' ':
+            new_string += " "
+
+    return new_string
+
+
+print(clean_up("---what's my +*& line?") == " what s my line ")
