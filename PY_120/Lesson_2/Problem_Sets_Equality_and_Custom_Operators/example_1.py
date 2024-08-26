88527a6e35b66708fbfe5f6a84397f858d08d3cc
Spicyblue <ochulor.okechukwu@gmail.com>
2024-08-26 13:49:44 +0200

    Solutions to problem set equality and custom operators

---
+++ PY_120/Lesson_2/Problem_Sets_Equality_and_Custom_Operators/example_1.py
@@ -0,0 +1,41 @@
+'''
+Example 1
+
+Name the method used to customize each of the following operators:
+
+Operator
+>
+*
+<=
+!=
++=
+**=
+//
+
+'''
+
+# Solution
+
+'''
+
+Operator            Method
+>                   __gt__
+*                   __mul__
+<=                  __le__
+!=                  __ne__
++=                  __iadd__
+**=                 __ipow__
+//                  __floordiv__
+
+'''
+
+## LS Solution ##
+
+# Operator          Method
+# >                 __gt__
+# *                 __mul__
+# <=                __le__
+# !=                __ne__
+# +=                __iadd__
+# **=               __ipow__
+# //                __floordiv__
