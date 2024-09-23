fe53047b40e36fde3e2c8dd30d503a58755a3fd5
ochuloro <ochulor.okechukwu@gmail.com>
2024-09-24 00:11:00 +0200

    Solutions to higher order function exercises

--- PY_130/Practice_Problems/Higher_Order_Functions/reduce.py
+++ PY_130/Practice_Problems/Higher_Order_Functions/reduce.py
@@ -54,7 +54,6 @@ You may use the following examples to test your code:
 def reduce(callback, iterable, starter):
     accum = starter
     for item in iterable:
-        print(accum)
         accum = callback(item, accum)
 
     return accum
@@ -63,11 +62,11 @@ numbers = (1, 2, 4, 8, 16)
 total = lambda number, accum: accum + number
 print(reduce(total, numbers, 0))        # 31
 
-# numbers = [10, 3, 5]
-# product = lambda number, accum: accum * number
-# print(reduce(product, numbers, 2))      # 300
+numbers = [10, 3, 5]
+product = lambda number, accum: accum * number
+print(reduce(product, numbers, 2))      # 300
 
-# colors = ['red', 'orange', 'yellow', 'green',
-#           'blue', 'indigo', 'violet']
-# rainbow = lambda color, accum: accum + color[0].upper()
-# print(reduce(rainbow, colors, ''))      # ROYGBIV
+colors = ['red', 'orange', 'yellow', 'green',
+          'blue', 'indigo', 'violet']
+rainbow = lambda color, accum: accum + color[0].upper()
+print(reduce(rainbow, colors, ''))      # ROYGBIV
