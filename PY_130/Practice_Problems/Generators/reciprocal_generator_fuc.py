5e45922230604a8ddc3aa77ec9470312686f83c2
ochuloro <ochulor.okechukwu@gmail.com>
2024-09-24 18:34:55 +0200

    Solutions to generator exercises

--- PY_130/Practice_Problems/Generators/reciprocal_generator_fuc.py
+++ PY_130/Practice_Problems/Generators/reciprocal_generator_fuc.py
@@ -3,6 +3,7 @@ Reciprocal generator function.
 Create a generator expression that generates the reciprocals of the numbers from 1 to 10.
 A reciprocal of a number n is 1 / n. Use a for loop to print each value.
 '''
+# Solution
 
 def reciprocal(num):
     
@@ -14,3 +15,20 @@ reciprocals_nums = reciprocal(10)
 
 for values in reciprocals_nums:
     print(values)
+
+## LS Solution ##
+# def reciprocals(n):
+#     number = 1
+#     while number <= n:
+#         yield 1 / number
+#         number += 1
+
+# for value in reciprocals(7):
+#     print(value)
+
+# def reciprocals(n):
+#     for number in range(1, n + 1):
+#         yield 1 / number
+
+# for value in reciprocals(7):
+#     print(value)
