295d025dc988b9ce7c314f134bffb5ae52c8d344
ochuloro <ochulor.okechukwu@gmail.com>
2025-01-07 18:02:48 +0100

    Summary of all SQL codes

---
+++ LS_180/SQL_fundamentals/Easy_1/summary.sql
@@ -0,0 +1,35 @@
+-- 1. Create a Table
+-- Now that we have an animals database, we can lay the groundwork needed to add some data to it.
+
+-- Make a table called birds. It should have the following fields:
+
+-- id (a primary key)
+-- name (string with space for up to 25 characters)
+-- age (integer)
+-- species (a string with room for no more than 15 characters)
+
+-- 2. Insert Data
+-- For this exercise, we'll add some data to our birds table. Add five records to this database so that our data looks like:
+
+
+-- id |   name   | age | species
+----+----------+-----+---------
+--  1 | Charlie  |   3 | Finch
+--  2 | Allie    |   5 | Owl
+--  3 | Jennifer |   3 | Magpie
+--  4 | Jamie    |   4 | Owl
+--  5 | Roy      |   8 | Crow
+
+-- 3. Select Data
+-- Write an SQL statement to query all data that is currently in our birds table.
+
+-- 4. WHERE Clause
+-- In this exercise, let's practice filtering the data we want to query. 
+-- Using a WHERE clause, SELECT records for birds under the age of 5.
+
+-- 5. Update Data
+-- It seems there was a mistake when we were inserting data in the birds table. 
+-- All the crows are actually ravens. 
+-- Update the birds table so that the rows with a species of 'Crow' now read 'Raven'.
+
+
