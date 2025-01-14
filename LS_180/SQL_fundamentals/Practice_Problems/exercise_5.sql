4db2e9b2109602bca9438efb7c22aecf65a2ed0f
ochuloro <ochulor.okechukwu@gmail.com>
2025-01-14 16:44:09 +0100

    Solution to exercises

---
+++ LS_180/SQL_fundamentals/Practice_Problems/exercise_5.sql
@@ -0,0 +1,5 @@
+-- Exercise 5
+-- Write a SQL statement to determine the average (mean) temperature
+-- (divide the sum of the high and low temperatures by two) for each day from
+-- March 2, 2016 through March 8, 2016. Make sure to round each average value to one decimal place.
+
