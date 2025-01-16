4f6bee861c5431156f36e0c622685894e033fb19
ochuloro <ochulor.okechukwu@gmail.com>
2025-01-16 17:23:44 +0100

    Solution to practice exercises on GroupBY and Aggregates problem set

---
+++ LS_180/SQL_fundamentals/GroupBy_And_Aggregates/exercise_11.sql
@@ -0,0 +1,22 @@
+-- Exercise 11
+
+-- Write a SQL query that will return the following data:
+
+-- Write a SQL query that will return the following data:
+
+--    genre   | total_duration
+-- -----------+----------------
+--  horror    |            113
+--  thriller  |            113
+--  action    |            132
+--  crime     |            175
+--  drama     |            198
+--  espionage |            245
+--  comedy    |            407
+--  scifi     |            632
+-- (8 rows)
+
+-- Solution
+
+SELECT genre, SUM(duration) as total_duration from films GROUP by genre ORDER BY total_duration
+ ASC;
