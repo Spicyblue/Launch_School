e51dc95333c9f8b8f16ecba2cf11426ee77e8454
ochuloro <ochulor.okechukwu@gmail.com>
2025-01-16 15:20:21 +0100

    Solution to practice exercises on GroupBY and Aggregates problem set

---
+++ LS_180/SQL_fundamentals/GroupBy_And_Aggregates/exercise_5.sql
@@ -0,0 +1,7 @@
+-- Exercise 5
+
+-- Write a SQL query that returns the same results as the answer for #3, but without using DISTINCT.
+
+-- Solution
+
+SELECT ROUND(AVG(duration)) as round FROM films;
